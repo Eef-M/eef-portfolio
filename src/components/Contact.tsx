57eae87949d65b6f80566d20adb5a0dea145808b
Eef-M <eefmekel@gmail.com>
2025-01-06 15:31:32 +0700

    feat: add form message functionality

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,67 @@
 import { Send } from "lucide-react";
 import { motion } from "motion/react";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast, ToastContainer } from "react-toastify";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    from_name: "",
+    from_email: "",
+    message: "",
+    to_name: "Eef Mekelliano",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+  const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+  const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Validasi input
+    if (!formData.from_name || !formData.from_email || !formData.message) {
+      toast.error("Please fill in all the fields.", { position: "top-right" });
+      return;
+    }
+
+    setIsLoading(true);
+
+    emailjs.send(serviceId, templateId, formData, publicKey).then(
+      () => {
+        toast.success("Message sent successfully! 🎉", {
+          position: "top-right",
+        });
+        setFormData({
+          from_name: "",
+          from_email: "",
+          message: "",
+          to_name: "Eef Mekelliano",
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        console.error("Error:", error);
+        toast.error("Failed to send message. Please try again. 😞", {
+          position: "top-right",
+        });
+        setIsLoading(false);
+      }
+    );
+  };
+
   return (
     <section id="contact" className="py-24 px-4 md:px-12">
+      <ToastContainer />
       <motion.div
         initial={{ opacity: 0, z: 100 }}
         whileInView={{
@@ -20,12 +78,14 @@ const Contact = () => {
           I'm always open to new opportunities and interesting projects
         </p>
 
-        <form className="mt-12 max-w-xl mx-auto">
+        <form className="mt-12 max-w-xl mx-auto" onSubmit={handleSubmit}>
           <div className="grid grid-cols-1 gap-6">
             <div>
               <input
                 type="text"
-                id="name"
+                name="from_name"
+                value={formData.from_name}
+                onChange={handleChange}
                 className="block w-full rounded-md border-2 border-eefPrimary bg-transparent px-4 py-2 placeholder:text-eefSecondary"
                 placeholder="Name"
               />
@@ -34,7 +94,9 @@ const Contact = () => {
             <div>
               <input
                 type="email"
-                id="email"
+                name="from_email"
+                value={formData.from_email}
+                onChange={handleChange}
                 className="block w-full rounded-md border-2 border-eefPrimary bg-transparent px-4 py-2 placeholder:text-eefSecondary"
                 placeholder="Email"
               />
@@ -42,19 +104,39 @@ const Contact = () => {
 
             <div>
               <textarea
-                id="message"
+                name="message"
                 rows={4}
+                value={formData.message}
+                onChange={handleChange}
                 className="block w-full rounded-md border-2 border-eefPrimary bg-transparent px-4 py-2 placeholder:text-eefSecondary"
                 placeholder="Message"
               />
             </div>
 
-            <button
+            {/* <button
               type="submit"
               className="flex justify-center items-center rounded-md bg-eefPrimary text-white py-3 gap-2 font-medium hover:bg-eefAccent transition duration-300 ease-in-out"
             >
               <Send className="w-5 h-5" />
               Send
+            </button> */}
+
+            <button
+              type="submit"
+              className={`flex justify-center items-center rounded-md py-3 gap-2 font-medium transition duration-300 ease-in-out ${
+                isLoading
+                  ? "bg-gray-500 cursor-not-allowed"
+                  : "bg-eefPrimary text-white hover:bg-eefAccent"
+              }`}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                "Sending..."
+              ) : (
+                <>
+                  <Send className="w-5 h-5" /> Send
+                </>
+              )}
             </button>
           </div>
         </form>
